8ec648c7e9515fc3bb193003a01278ce44986753
Soham <sovaramc323@gmail.com>
2026-09-12 18:34:39 +0530

    Add back guide import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import chestGuide from "./assets/chest-guide.jpg";
+import backGuide from "./assets/back-guide.jpg";
 import benchPress from "./assets/exercises/bench-press.jpg";
 import cableFly from "./assets/exercises/cable-fly.jpg";
 import inclineDumbbellPress from "./assets/exercises/incline-dumbbell-press.jpg";
